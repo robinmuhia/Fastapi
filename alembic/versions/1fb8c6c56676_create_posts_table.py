8c5528347e952fc8d687254c68ee69253be5dfcc
Robin <muhiarobinonyancha@gmail.com>
2023-01-16 17:02:04 +0300

    Initial commit

---
+++ alembic/versions/1fb8c6c56676_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: 1fb8c6c56676
+Revises: 
+Create Date: 2023-01-16 17:29:48.606502
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1fb8c6c56676'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/routers/user.py
@@ -0,0 +1,54 @@
+from .. import models,schemas,utils,oauth2
+from fastapi import status,HTTPException,Response,Depends,APIRouter
+from sqlalchemy.orm import Session
+from ..database import get_db
+
+
+router = APIRouter(
+    prefix="/users",
+    tags = ['Users']
+)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
+def create_user(user:schemas.UserCreate,db:Session = Depends(get_db)):
+    user_exists = db.query(models.User).filter(models.User.email == user.email).first()
+    if user_exists:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f'Email {user.email} is already in use')
+
+
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get('/{id}',status_code=status.HTTP_200_OK,response_model=schemas.UserOut)
+def get_user(id:int,db:Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {id} does not exits')
+    return user
+
+
+@router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id:int,db:Session = Depends(get_db),current_user:int = Depends(oauth2.get_current_user)):
+    user_query = db.query(models.User).filter(models.User.id == id)
+    deleted_user = user_query.first()
+    if deleted_user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id: {id} does not exist')
+
+    if deleted_user.id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Not authorized to perform requested action')
+
+    user_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
